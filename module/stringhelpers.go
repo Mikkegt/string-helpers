94ef8e8a5b5d9a8a2416112abaec41d2c751f89d
Kawano <kawano.misato@gmail.com>
2020-10-24 02:26:20 +0900

    create Interleave

--- module/stringhelpers.go
+++ module/stringhelpers.go
@@ -14,3 +14,30 @@ func Reverse(s string) (r string) {
 	}
 	return
 }
+
+func Interleave(s []string) (r string) {
+	// need?
+	str := make([]string, len(s))
+	copy(str, s)
+	var maxLen int
+
+	for _, v := range str {
+		if maxLen < len([]rune(v)) {
+			maxLen = len([]rune(v))
+		}
+	}
+
+	var result string
+	for i := 0; i < maxLen; i++ {
+		for j := 0; j < len(str); j++ {
+			for _, v := range str[j] {
+				result = result + string(v)
+				break
+			}
+			if len([]rune(str[j])) != 0 {
+				str[j] = string([]rune(str[j])[1:])
+			}
+		}
+	}
+	return result
+}
--- module/stringhelpers_test.go
+++ module/stringhelpers_test.go
@@ -15,3 +15,10 @@ func TestReverse(t *testing.T) {
 		t.Error("Expected エマロ・エマルテ, got", v)
 	}
 }
+
+func TestInterleave(t *testing.T) {
+	v := Interleave([]string{"地上波", "ダメ！b", "絶対！"})
+	if v != "地ダ絶上メ対波！！b" {
+		t.Error("Expected 地ダ絶上メ対波！！, got", v)
+	}
+}
